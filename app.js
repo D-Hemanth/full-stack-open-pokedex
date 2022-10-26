1d51a336b1b1b248e63857899ba4aa1b5a0bf140
Hemanth-D <hemanthh531@gmail.com>
2022-10-26 19:11:37 +0530

    part11 11.11: healthcheck deploy

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+// a simple endpoint for doing an application health check to the backend
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
+// a dummy endpoint in the app that makes it possible to do some code changes and to ensure that the deployed version has really changed
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
